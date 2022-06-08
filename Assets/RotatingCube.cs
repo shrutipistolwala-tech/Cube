157d46228f2bd3a9310d4df16d1a2a596aef8772
Shruti Pistolwala <shruti.pistolwala@tum.de>
2022-06-08 10:42:53 +0200

    rotating cube

---
+++ Assets/RotatingCube.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingCube : MonoBehaviour
+{
+    public Vector3 editRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(editRotation);
+    }
+}
